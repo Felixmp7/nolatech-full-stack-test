aaaf8580144f5199e3041091c8b6da381d1395ce
Felix Pacheco <devfex.19@gmail.com>
2025-02-17 15:12:33 -0400

    Refactor authentication controller to include error handling and improve response structure

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,36 +3,49 @@ import jwt from 'jsonwebtoken';
 
 import { Employee } from '../models/employee.model.js';
 import { User } from '../models/user.model.js';
+import { ERRORS, handleErrors } from '../utils/errorHandler.utils.js';
 
 const createUser = async ({ email, password, role }) => {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ email, password: hashedPassword, role });
-    await user.save();
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ email, password: hashedPassword, role });
+        await user.save();
+        return user;
+    } catch (error) {
+       throw new Error(error);
+    }
 
-    return user;
 };
 
 const createEmployee = async ({ userId, fullName, position }) => {
-    const employee = new Employee({ userId, fullName, position });
-    await employee.save();
-    return employee;
+     try {
+         const employee = new Employee({ userId, fullName, position });
+         await employee.save();
+         return employee;
+     } catch (error) {
+        throw new Error(error);
+     }
 }
 
 export const register = async (req, res) => {
     try {
         const { email, password, role, fullName, position } = req.body;
+        if (!email || !password || !role || !fullName || !position) throw new Error(ERRORS.BAD_REQUEST);
+
         const user = await createUser({ email, password, role });
         const employee = await createEmployee({ userId: user._id, fullName, position });
-        res.status(201).json({ message: 'Usuario registrado ✅', user, employee });
+
+        res.status(201).json({ message: 'Usuario registrado ✅', user: { id: user._id, email, role }, employee });
     } catch (error) {
-        console.log(error)
-        res.status(500).json({ error: 'Error en el registro ❌' });
+        // console.log({ error, message: error.message, name: error.name, log: "register" });
+        handleErrors(error, res);
     }
 };
 
 export const login = async (req, res) => {
     try {
         const { email, password } = req.body;
+        if (!email || !password) throw new Error(ERRORS.BAD_REQUEST);
         const user = await User.findOne({ email });
         if (!user || !await bcrypt.compare(password, user.password)) {
             return res.status(401).json({ error: 'Credenciales inválidas ❌' });
@@ -40,6 +53,7 @@ export const login = async (req, res) => {
         const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
         res.status(200).json({ token });
     } catch (error) {
-        res.status(500).json({ error: 'Error en el login' });
+        // console.log({ error, message: error.message, name: error.name, log: "register" });
+        handleErrors(error, res);
     }
 };
