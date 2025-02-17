a83180aebd03fa6006963a82f094a736b2442a66
Felix Pacheco <devfex.19@gmail.com>
2025-02-17 15:11:56 -0400

    Add employee routes to server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 import authRoutes from './routes/auth.routes.js';
+import employeeRoutes from './routes/employees.routes.js';
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use(express.json());
 
 
 app.use('/api/auth', authRoutes);
+app.use('/api/employees', employeeRoutes);
 
 
 // Conectar a MongoDB
