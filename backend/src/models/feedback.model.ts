33d85a83a6ce8e3f045f88659b46988fcdc2cddd
Felix Pacheco <devfex.19@gmail.com>
2025-02-17 14:12:48 -0400

    Remove Mongoose models for Employee, Evaluation, and Feedback

--- backend/src/models/feedback.model.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const FeedbackSchema = new mongoose.Schema({
-    evaluation: { type: mongoose.Schema.Types.ObjectId, ref: 'Evaluation', required: true },
-    comment: { type: String, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-}, { timestamps: true });
-
-export const Feedback = mongoose.model('Feedback', FeedbackSchema);
