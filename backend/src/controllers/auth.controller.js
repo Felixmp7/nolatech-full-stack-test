3358411038b170d18a67985aac1006d4cfbd16a9
Felix Pacheco <devfex.19@gmail.com>
2025-02-17 14:12:21 -0400

    Migrate backend to javascript

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,45 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+import { Employee } from '../models/employee.model';
+import { User } from '../models/user.model';
+
+const createUser = async ({ email, password, role }) => {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ email, password: hashedPassword, role });
+    await user.save();
+
+    return user;
+};
+
+const createEmployee = async ({ userId, fullName, position }) => {
+    const employee = new Employee({ userId, fullName, position });
+    await employee.save();
+    return employee;
+}
+
+export const register = async (req, res) => {
+    try {
+        const { email, password, role, fullName, position } = req.body;
+        const user = await createUser({ email, password, role });
+        const employee = await createEmployee({ userId: user._id, fullName, position });
+        res.status(201).json({ message: 'Usuario registrado ✅', user, employee });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'Error en el registro ❌' });
+    }
+};
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user || !await bcrypt.compare(password, user.password)) {
+            return res.status(401).json({ error: 'Credenciales inválidas ❌' });
+        }
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).json({ error: 'Error en el login' });
+    }
+};
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,10 +1,9 @@
-import mongoose from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+const UserSchema = new Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['Admin', 'Manager', 'Employee'], required: true }
 }, { timestamps: true });
 
-export const User = mongoose.model('User', UserSchema);
+export const User = model('User', UserSchema);
