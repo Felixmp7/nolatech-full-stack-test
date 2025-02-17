1708ce047dc7d3c22e40028066e5dbc8bb6a1362
Felix Pacheco <devfex.19@gmail.com>
2025-02-17 15:12:09 -0400

    Add error handling utilities for standardized HTTP responses

---
+++ backend/src/utils/errorHandler.utils.js
@@ -0,0 +1,20 @@
+import { badRequest, forbidden, notFound, unauthorized } from "./httpResponse.utils.js";
+
+export const ERRORS = {
+    BAD_REQUEST: 'BadRequest',
+    NOT_FOUND: 'NotFound',
+    FORBIDDEN: 'Forbidden',
+    UNAUTHORIZED: 'Unauthorized'
+}
+
+export const handleErrors =  (error, res) => {
+    if (error.message === ERRORS.BAD_REQUEST) return badRequest(res);
+
+    if (error.message === ERRORS.NOT_FOUND) return notFound(res);
+
+    if (error.message === ERRORS.FORBIDDEN) return forbidden(res);
+
+    if (error.message === ERRORS.UNAUTHORIZED) return unauthorized(res);
+
+    return res.status(500).json({ message: 'Internal server error' });
+}
