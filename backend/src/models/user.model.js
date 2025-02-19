8f9a3edafbd3998eb4cd37c4daf45c3df49fe16b
Felix Pacheco <devfex.19@gmail.com>
2025-02-19 15:10:55 -0400

    Update user model to use dynamic role validation and improve code readability

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,9 +1,15 @@
 import { Schema, model } from 'mongoose';
 
+import { VALID_ROLES } from '../validations/register.validation.js';
+
 const UserSchema = new Schema({
-    email: { type: String, unique: true, required: true },
+    email: {
+        type: String, unique: true, required: true 
+    },
     password: { type: String, required: true },
-    role: { type: String, enum: ['Admin', 'Manager', 'Employee'], required: true }
+    role: {
+        type: String, enum: VALID_ROLES, required: true 
+    }
 }, { timestamps: true });
 
 export const User = model('User', UserSchema);
