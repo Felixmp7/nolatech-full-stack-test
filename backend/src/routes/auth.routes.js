3a16e4ef7bae20be666e60a88b5167181f8ff796
Felix Pacheco <devfex.19@gmail.com>
2025-02-19 15:11:02 -0400

    Enhance authentication routes with input validation middleware for registration and login

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+
 import { login, register } from '../controllers/auth.controller.js';
+import { validateInputs } from '../middlewares/validate.middleware.js';
+import { validateCredentials } from '../validations/login.validation.js';
+import { validateRegister } from '../validations/register.validation.js';
 
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validateRegister(), validateInputs, register);
+router.post('/login', validateCredentials(), validateInputs, login);
 
 export default router;
