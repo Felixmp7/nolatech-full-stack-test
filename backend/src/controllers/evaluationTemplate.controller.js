d05265b394e53ce9a97acc4b3f12c841bccf1123
Felix Pacheco <devfex.19@gmail.com>
2025-02-19 15:10:33 -0400

    Refactor evaluation template controller to use matchedData for input validation and improve error handling

--- backend/src/controllers/evaluationTemplate.controller.js
+++ backend/src/controllers/evaluationTemplate.controller.js
@@ -1,41 +1,60 @@
+import { matchedData } from 'express-validator';
+
+import { ERRORS } from '../constants/errors.js';
 import { EvaluationTemplate } from '../models/evaluationTemplate.model.js';
-import { ERRORS, handleErrors } from '../utils/errorHandler.utils.js';
+import { handleErrors } from '../utils/errorHandler.utils.js';
 
 export const createEvaluation = async (req, res) => {
-    const { title, description, questions } = req.body;
+    const {
+        title, description, questions
+    } = matchedData(req);
     try {
-        if (!title || !description || !questions.length) throw new Error(ERRORS.BAD_REQUEST);
-        const newEvaluation = new EvaluationTemplate({ title, description, questions });
+        const newEvaluation = new EvaluationTemplate({
+            title, description, questions
+        });
         await newEvaluation.save();
-        res.status(201).json({ message: 'Evaluation Template created successfully ✅', newEvaluationTemplate: newEvaluation });
+        res.status(201).json({
+            message: 'Evaluation Template created successfully ✅',
+            newEvaluationTemplate: newEvaluation
+        });
     } catch (error) {
-        console.log({ error, errors: error.errors, message: error.message, name: error.name, log: 'createEvaluation' });
+        console.log({
+            error,
+            errors: error.errors,
+            message: error.message,
+            name: error.name,
+            log: 'createEvaluation'
+        });
         handleErrors(error, res);
     }
 };
 
 export const getEvaluationById = async (req, res) => {
-    const { id } = req.params;
+    const { id } = matchedData(req, { locations: ['params'] });
     try {
         const evaluation = await EvaluationTemplate.findById(id);
         if (!evaluation) throw new Error(ERRORS.NOT_FOUND);
         res.status(200).json(evaluation);
     }
     catch (error) {
-        console.log({ error, errors: error.errors, message: error.message, name: error.name, log: 'createEvaluation' });
+        console.log({
+            error, errors: error.errors, message: error.message, name: error.name, log: 'createEvaluation'
+        });
         handleErrors(error, res);
     }
 };
 
 export const updateEvaluationById = async (req, res) => {
-    const { id } = req.params;
-    const { title, description, questions } = req.body;
+    const { id } = matchedData(req, { locations: ['params'] });
+    const {
+        title, description, questions
+    } = matchedData(req);
     try {
-        if (!title || !description || !questions?.length) throw new Error(ERRORS.BAD_REQUEST);
-
         const updatedEvaluation = await EvaluationTemplate.findByIdAndUpdate(
             id,
-            { title, description, questions },
+            {
+                title, description, questions
+            },
             { new: true }
         );
         if (!updatedEvaluation) throw new Error(ERRORS.NOT_FOUND);
