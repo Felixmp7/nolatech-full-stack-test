dd8c14dd2722a48ff6a0ad2657f08fee1536ea67
Felix Pacheco <devfex.19@gmail.com>
2025-02-19 11:01:06 -0400

    Fix response status code in getEmployees controller

--- backend/src/controllers/employee.controller.js
+++ backend/src/controllers/employee.controller.js
@@ -3,7 +3,7 @@ import { Employee } from '../models/employee.model.js';
 export const getEmployees = async (_req, res) => {
     try {
         const employees = await Employee.find();
-        res.status(201).json({ employees });
+        res.status(200).json({ employees });
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: 'Error when getting employees ❌' });
