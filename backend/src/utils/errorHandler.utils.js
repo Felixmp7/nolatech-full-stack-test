0a45097932463f10c848939cb013f34ef15b4d43
Felix Pacheco <devfex.19@gmail.com>
2025-04-14 17:02:01 -0400

    refactor: reorganize error handling logic and improve response structure in handleErrors function

--- backend/src/utils/errorHandler.utils.js
+++ backend/src/utils/errorHandler.utils.js
@@ -1,17 +1,25 @@
 import { ERRORS, MONGO_ERRORS } from '../constants/errors.js';
 import {
-    forbidden, notFound, unauthorized 
+    badRequest,
+    forbidden,
+    internalServerError,
+    notFound,
+    returnAPIResponse,
+    unauthorized
 } from './httpResponse.utils.js';
 
+// eslint-disable-next-line complexity
 export const handleErrors =  (error, res) => {
-
     if (error.message === ERRORS.NOT_FOUND) return notFound(res);
-
     if (error.message === ERRORS.FORBIDDEN) return forbidden(res);
 
     if (error.message === ERRORS.UNAUTHORIZED) return unauthorized(res);
 
-    if (error.code === MONGO_ERRORS.DUPLICATE_KEY) return res.status(409).json({ message: `Duplicated resource ${JSON.stringify(error.keyValue)}` });
+    if (error.message === ERRORS.INVALID_CREDENTIALS) return badRequest(res, 'Invalid Credentials!');
 
-    return res.status(500).json({ message: 'Internal server error' });
+    if (error.code === MONGO_ERRORS.DUPLICATE_KEY) return res.status(409).json(returnAPIResponse({
+        message: [`Duplicated resource ${JSON.stringify(error.keyValue)}`]
+    }));
+
+    return internalServerError(res, [error.message]);
 };
