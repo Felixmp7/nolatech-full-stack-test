42ef04fc4cd42c648bd7614664152d1a47b55eb2
Felix Pacheco <devfex.19@gmail.com>
2025-04-14 12:42:57 -0400

    feat: enhance LoginForm component with form.Field and InputLabel for better structure and usability

--- frontend/src/components/forms/login.form.tsx
+++ frontend/src/components/forms/login.form.tsx
@@ -1,17 +1,20 @@
 import { NavLink } from "react-router";
 
-
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { cn } from "@/lib/utils";
+import { useLoginForm } from "@/hooks/forms/useLoginForm";
+import { cn } from "@/utils/tw-merge.utils";
+import { InputLabel } from "../ui/input.label";
 
 export function LoginForm({
     className,
     ...props
 }: React.ComponentPropsWithoutRef<"form">) {
+    const { form, handleSubmit } = useLoginForm()
     return (
-        <form className={cn("flex flex-col gap-6", className)} {...props}>
+        <form
+            noValidate
+            className={cn("flex flex-col gap-6", className)} {...props}
+            onSubmit={handleSubmit}>
             <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-2xl font-bold">Login to your account</h1>
                 <p className="text-balance text-sm text-muted-foreground">
@@ -19,25 +22,55 @@ export function LoginForm({
                 </p>
             </div>
             <div className="grid gap-6">
-                <div className="grid gap-2">
-                    <Label htmlFor="email">Email</Label>
-                    <Input id="email" type="email" placeholder="m@example.com" required />
-                </div>
-                <div className="grid gap-2">
-                    <div className="flex items-center">
-                        <Label htmlFor="password">Password</Label>
-                        <a
-                            href="#"
-                            className="ml-auto hidden text-sm underline-offset-4 hover:underline"
+                <form.Field
+                    name="email"
+                    children={(field) => (
+                        <InputLabel
+                            label="Email"
+                            labelProps={{  htmlFor: "email" }}
+                            inputProps={{
+                                id:"email",
+                                type:"email",
+                                placeholder:"m@example.com",
+                                value: field.state.value,
+                                onBlur: field.handleBlur,
+                                onChange: (e) => field.handleChange(e.target.value)
+                            }}
+                        />
+                    )}
+                />
+                <form.Field
+                    name="password"
+                    children={(field) => (
+                        <InputLabel
+                            label="Password"
+                            labelProps={{  htmlFor: "password" }}
+                            inputProps={{
+                                id:"password",
+                                type:"password",
+                                placeholder:"*******",
+                                value: field.state.value,
+                                onBlur: field.handleBlur,
+                                onChange: (e) => field.handleChange(e.target.value)
+                            }}
                         >
+                            <a
+                                href="#"
+                                className="ml-auto text-sm underline-offset-4 hover:underline"
+                            >
                           Forgot your password?
-                        </a>
-                    </div>
-                    <Input id="password" type="password" required placeholder="*******" />
-                </div>
-                <Button type="submit" className="w-full">
-                  Login
-                </Button>
+                            </a>
+                        </InputLabel>
+                    )}
+                />
+                <form.Subscribe
+                    selector={(state) => [state.canSubmit, state.isSubmitting]}
+                    children={([canSubmit, isSubmitting]) => (
+                        <Button type="submit" className="w-full" disabled={!canSubmit}>
+                            {isSubmitting ? '...' : 'Login'}
+                        </Button>
+                    )}
+                />
             </div>
             <div className="text-center text-sm">
                 Don&apos;t have an account?{" "}
