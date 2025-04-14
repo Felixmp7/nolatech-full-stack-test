861573d94fc223e6d965bbc3e5e95389a8572548
Felix Pacheco <devfex.19@gmail.com>
2025-04-14 12:42:44 -0400

    feat: refactor routing to use createBrowserRouter and add authentication loader

--- frontend/src/app/router/index.tsx
+++ frontend/src/app/router/index.tsx
@@ -1,12 +1,31 @@
-import { Route, Routes } from "react-router";
+import {
+    createBrowserRouter,
+    redirect,
+} from "react-router";
 
 import { LoginPage } from "@/app/login/page";
 import { SignUpPage } from "@/app/signup/page";
+import { PATHS } from "@/constants";
+import { isLoggedIn } from "@/utils/localstorage.utils";
 
-export const Router = () => (
-    <Routes>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="signup" element={<SignUpPage />} />
-    </Routes>
-
-)
+export const router = createBrowserRouter([
+    {
+        index: true,
+        element: <div>HOME</div>,
+    },
+    {
+        path: PATHS.login,
+        loader: () => {
+            const auth = isLoggedIn();
+            if (auth) {
+                return redirect(PATHS.root);
+            }
+            return '...loading'
+        },
+        Component: LoginPage,
+    },
+    {
+        path: PATHS.signup,
+        Component: SignUpPage,
+    },
+]);
